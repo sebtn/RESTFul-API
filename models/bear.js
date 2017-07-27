9970352ad220ea7b2099966980711be2677297f6
sebtn <sdebedout@gmail.com>
2017-07-27 00:39:43 -0400

    Mongo connected

--- models/bear.js
+++ models/bear.js
@@ -1,8 +1,10 @@
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema 
 
-var BearSchema = new Schema({
+const BearSchema = new Schema({
   name: String
 })
 
-module.exports = mongoose.model("Bear", BearSchema)
+const bearClass = mongoose.model("Bear", BearSchema)
+
+module.exports = bearClass
--- server.js
+++ server.js
@@ -9,20 +9,50 @@ var Bear = require('./models/bear.js')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-var port = process.env.PORT || 3000 
+var port = process.env.PORT || 3300 
 
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o')
+// mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o')
+mongoose.connect('mongodb://localhost:/api/api') // create db
 
 /*Routes API*/
 var router = express.Router()
 
+/*-----------------------------------------------------*/
 /*Middleware for all requests*/
 router.use(function(req, res, next) {
   console.log('Something is here!')
   next()
 })
 
+/*API
+/*-----------------------------------------------------
+* localhost/api/bear
+*/
+router.route('/bears')
+
+/*-----------------------------------------------------*/
+/*Create a bear*/
+  .post(function(req, res) {
+    var bear = new Bear()
+    bear.name = req.body.name // create name prop
+
+    bear.save(function(err) {
+      if (err) { res.send(err) }
+      res.json({message: 'Bear Created!'})
+    })
+  })
+  
+/*-----------------------------------------------------*/
+/*Get all bears*/
+  .get(function(req, res) {
+    Bear.find(function(err, bears) {
+      if (err) { res.send(err) }
+      res.json(bears)
+    })
+  })
+
+/*-----------------------------------------------------*/
 /*Route Test*/
 router.get('/', function(req, res) {
   res.json({ message:'Hi, welcome to API' })
